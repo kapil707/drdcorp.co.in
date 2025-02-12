1ad7d277efb4d2adb14a99aab13699c3dfe04380
kapilji <kapil707sharma@gmail.com>
2025-02-12 16:15:48 +0530

    add

--- application/models/model-drdcorp/CorporateReport.php
+++ application/models/model-drdcorp/CorporateReport.php
@@ -51,7 +51,7 @@ class CorporateReport extends CI_Model
 	function sendReport() {
 		$this->db->select('*');
         $this->db->from('tbl_corporate_report');
-        $this->db->where('tbl_corporate_other.daily_status', 0);
+        $this->db->where('tbl_corporate_other.report_status', 0);
         $this->db->limit(10);
 
         $query = $this->db->get();
@@ -59,13 +59,20 @@ class CorporateReport extends CI_Model
 		foreach($result as $row)
 		{
 			/************************************************/
-			$memail     = $row->memail;
+			$email     = $row->email;
             $code       = $row->code;
             $compcode   = $row->compcode;
             $division   = $row->division;
-            $company_name = $row->company_full_name;
+            $company_name = $row->company_name;
+
+			$file1 = $file2 = $file3 = "1";
+			$date = date('Y-m-d');
             
             $this->get_body($date,$email,$code,$compcode,$division,$name,$company_name,$file1,$file2,$file3);
+
+			$dt = array('report_status'=>1);
+			$where = array('code'=>$code);
+			$this->CorporateReport->UpdateRecoreds("tbl_corporate_report", $dt, $where);
 		}
 	}
 
@@ -93,13 +100,12 @@ class CorporateReport extends CI_Model
 			$url3 = "<a href='".$url."'>".$file_name3."</a><br><br>";
 		}
 
-		$subject = "Daily Report (".$today_date.") ".ucwords(strtolower($company_full_name))." (".$division.")";
-		$message = "Sir<br>It is the sales data as you have sought<br>";
+		$subject = "Daily Report (".$today_date.") ".ucwords(strtolower($company_name))." (".$division.")";
+		$message = "Sir $name,<br>It is the sales data as you have sought<br>";
 		$message.= $url1;
 		$message.= $url2;
 		$message.= $url3;
 		$message.= "Please download the file as in the links above for your reference.<br>Thanks and regards<br><br>D.R. Distributors Pvt Ltd<br>Notice & Disclaimer - This email and any files transmitted with it contain Proprietary, privileged and confidential information and/or information protected by intellectual property rights and is only for the use of the intended recipient of this message. If you are not the intended recipient, please delete or destroy this and all copies of this message along with the attachments immediately. You are hereby notified and directed that (1) if you are not the named and intended addressee you shall not disseminate, distribute or copy this e-mail, and (2) any offer for product/service shall be subject to a final evaluation of relevant patent status. Company cannot guarantee that e-mail communications are secure or error-free, as information could be intercepted, corrupted, amended, lost, destroyed, arrive late or incomplete, or may contain viruses. Company does not accept responsibility for any loss or damage arising from the use of this email or attachments.";
-
 		$this->test_email($email,$subject,$message);
 	}
 
@@ -107,9 +113,9 @@ class CorporateReport extends CI_Model
 	{
 		$email = $this->phpmailer_lib->load();
 		
-		$addreplyto 		= "application@drdistributor.com";
-		$addreplyto_name 	= "Vipul DRD";
-		$server_email 		= "application@drdistributor.com";
+		$addreplyto 		= "application@drdcorp.co.in";
+		$addreplyto_name 	= "Vipul Gupta";
+		$server_email 		= "application@drdcorp.co.in";
 		//$server_email 	= "send@drdindia.com";
 		$server_email_name 	= "DRD Corporate Report";
 		$email1 			= "kapildrd@gmail.com";
