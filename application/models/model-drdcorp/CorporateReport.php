5b0054e0930150ceb904657c3ed263b7d83bd7ec
kapilji <kapil707sharma@gmail.com>
2025-02-13 14:20:46 +0530

    add

--- application/models/model-drdcorp/CorporateReport.php
+++ application/models/model-drdcorp/CorporateReport.php
@@ -3,6 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class CorporateReport extends CI_Model
 {
 	public $myemail;
+	public $db;
 	public function __construct(){
 		parent::__construct();
 		
