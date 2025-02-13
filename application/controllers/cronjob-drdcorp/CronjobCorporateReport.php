f104a818d121dc85292cd0bfac91ca9f34b88c0b
kapilji <kapil707sharma@gmail.com>
2025-02-13 12:35:00 +0530

    add

--- application/controllers/cronjob-drdcorp/CronjobCorporateReport.php
+++ application/controllers/cronjob-drdcorp/CronjobCorporateReport.php
@@ -72,6 +72,47 @@ class CronjobCorporateReport extends CI_Controller
 		$email = $this->phpmailer_lib->load();
 		
 		$subject = "drd local test ok";
+		$message = $this->get_body();
+		
+		$addreplyto 		= "application@drdistributor.com";
+		$addreplyto_name 	= "Vipul DRD";
+		$server_email 		= "application@drdistributor.com";
+		//$server_email 	= "send@drdindia.com";
+		$server_email_name 	= "DRD TEST";
+		$email1 			= "kapil707sharma@gmail.com";
+		
+		$email->AddReplyTo($addreplyto,$addreplyto_name);
+		$email->SetFrom($server_email,$server_email_name);
+		$email->AddAddress($email1);
+		
+		$email->Subject   	= $subject;
+		$email->Body 		= $message . time();
+
+		$email->IsHTML(true);
+
+		$email->isSMTP();
+		$email->Host       = 'mail.drdcorp.co.in'; // SMTP Server
+		$email->SMTPAuth   = true; // Enable SMTP Authentication
+		$email->Username   = 'report@drdcorp.co.in'; // SMTP Username
+		$email->Password   = 'Kapil1234!@#$'; // SMTP Password
+		$email->SMTPSecure = 'ssl'; // Use SSL (as your SMTP port is 465)
+		$email->Port       = 465; // SMTP Port
+
+		if($email->send()){
+			echo 'Message has been sent';
+		}else{
+			echo 'Message could not be sent.';
+			echo 'Mailer Error: ' . $email->ErrorInfo;
+		}
+		echo "<pre>";
+		print_r($email);
+	}
+
+	public function test_email3()
+	{
+		$email = $this->phpmailer_lib->load();
+		
+		$subject = "drd local test ok";
 		$message = "drd local test ok";
 		
 		$addreplyto 		= "report@drdcorp.co.in";
