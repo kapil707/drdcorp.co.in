c4951e0571530fe158c3e90abf2332fc00c23dde
kapilji <kapil707sharma@gmail.com>
2025-02-13 14:04:07 +0530

    add

--- application/controllers/exe-drdcorp/ExeCorporate.php
+++ application/controllers/exe-drdcorp/ExeCorporate.php
@@ -29,50 +29,45 @@ class ExeCorporate extends CI_Controller
 			foreach ($data as $record) {
 				$code_array[] = $record['code'];
 				
+				$report_type = $record['report_type'];
 				$code = $record['code'];
 				$compcode = $record['compcode'];
+				$division = $record['division'];
+				$company_name = $record['company_name'];
 				$name = $record['name'];
 				$email = $record['email'];
-				$mobile = $record['mobile'];
-				$company_name = $record['company_name'];
-
-				$item_wise_report_daily_email = $record['item_wise_report_daily_email'];
-				$chemist_wise_report_daily_email = $record['chemist_wise_report_daily_email'];
-				$stock_and_sales_analysis_daily_email = $record['stock_and_sales_analysis_daily_email'];
-				$item_wise_report_monthly_email = $record['item_wise_report_monthly_email'];
-				$chemist_wise_report_monthly_email = $record['chemist_wise_report_monthly_email'];
-				$stock_and_sales_analysis_report_monthly_email = $record['stock_and_sales_analysis_report_monthly_email'];
+				$file1 = $record['file1'];
+				$file2 = $record['file2'];
+				$file3 = $record['file3'];
 
 				$insert_time = date('Y-m-d,H:i');
 
 				$dt = array(
+					'report_type' => $report_type,
 					'code' => $code,
 					'compcode' => $compcode,
+					'division' => $division,
+					'company_name' => $company_name,
 					'name' => $name,
 					'email' => $email,
-					'mobile' => $mobile,
-					'company_name' => $company_name,
-					'item_wise_report_daily_email' => $item_wise_report_daily_email,
-					'chemist_wise_report_daily_email' => $chemist_wise_report_daily_email,
-					'stock_and_sales_analysis_daily_email' => $stock_and_sales_analysis_daily_email,
-					'item_wise_report_monthly_email' => $item_wise_report_monthly_email,
-					'chemist_wise_report_monthly_email' => $chemist_wise_report_monthly_email,
-					'stock_and_sales_analysis_report_monthly_email' => $stock_and_sales_analysis_report_monthly_email,
+					'file1' => $file1,
+					'file2' => $file2,
+					'file3' => $file3,
 					'insert_time' => $insert_time,
 				);
 
 				if (!empty($code)) {
 					// Check karo agar record already exist karta hai
-					$existing_record = $this->CorporateReport->CheckRecoreds("tbl_corporate_report", array('code' => $code));
+					//$existing_record = $this->CorporateReport->CheckRecoreds("tbl_corporate_report", array('code' => $code));
 			
-					if ($existing_record) {
+					/*if ($existing_record) {
 						// Agar record exist karta hai to update karo
 						$where = array('code' => $code);
 						$this->CorporateReport->UpdateRecoreds("tbl_corporate_report", $dt, $where);
-					} else {
+					} else {*/
 						// Agar record exist nahi karta hai to insert karo
 						$this->CorporateReport->InsertRecoreds("tbl_corporate_report", $dt);
-					}
+					//}
 				}
 			}
 			$commaSeparatedString = implode(',', $code_array);
