7e02cbd660934a48a7976f3a11c511dd529f223e
kapilji <kapil707sharma@gmail.com>
2025-03-31 15:14:13 +0530

    add

--- application/models/model-bank/BankInvoiceModel.php
+++ application/models/model-bank/BankInvoiceModel.php
@@ -251,63 +251,59 @@ class BankInvoiceModel extends CI_Model
 		);
 
 		$response = curl_exec($curl);
-		print_r($response);
+		//print_r($response);
 		curl_close($curl);
 
 		$data1 = json_decode($response, true);
 
-		print_r($data1);
-		die();
+		//print_r($data1);
 		if (isset($data1['items'])) {
 			foreach ($data1['items'] as $message) {
-				$message_id = isset($message['message_id']) ? $message['message_id'] : "Body not found";
-				
-				$body = isset($message['body']) ? $message['body'] : "Body not found";
-				$date = isset($message['date']) ? $message['date'] : "Date not found";
-				$extracted_text = isset($message['extracted_text']) ? $message['extracted_text'] : "extracted_text not found";
-				$from_number = isset($message['from_number']) ? $message['from_number'] : "Date not found";
-				$ist_timestamp = isset($message['ist_timestamp']) ? $message['ist_timestamp'] : "timestamp not found";
-				$screenshot_image = isset($message['screenshot_image']) ? $message['screenshot_image'] : "screenshot_image not found";
-				$sender_name_place = isset($message['sender_name_place']) ? $message['sender_name_place'] : "sender_name_place not found";
-				$timestamp = isset($message['timestamp']) ? $message['timestamp'] : "timestamp not found";
-				$vision_text = isset($message['vision_text']) ? $message['vision_text'] : "vision_text not found";	
-				
-				$reply_id = isset($message["quoted_text"]) ? $message["quoted_text"] : "0";
-
-				//$extracted_text = str_replace("\n", "<br>", $extracted_text);
-				//$vision_text = str_replace("\n", "<br>", $vision_text);
-
-				//$date = date('Y-m-d H:i:s', strtotime($date));
-				// Convert date
-				$date = new DateTime($date);
-				// Format as "YYYY-MM-DD"
-				$date = $date->format("Y-m-d");
+				$mtime = isset($message['mtime']);
+				$dispatchtime = isset($message['dispatchtime']);
+				$date = isset($message['date']);
+				$vno = isset($message['vno']);
+				$tagno = isset($message['tagno']);
+				$gstvno = isset($message['gstvno']);
+				$pickedby = isset($message['pickedby']);
+				$checkedby = isset($message['checkedby']);
+				$deliverby = isset($message['deliverby']);
+				$amt = isset($message['amt']);
+				$taxamt = isset($message['taxamt']);
+				$acno = isset($message['acno']);
+				$chemist_id = isset($message['chemist_id']);
+				$status = isset($message['status']);
+				$insert_time = isset($message['insert_time']);
 
 				$dt = array(
-					'message_id' => $message_id,
-					'body' => $body,
+					'mtime' => $mtime,
+					'dispatchtime' => $dispatchtime,
 					'date' => $date,
-					'extracted_text' => $extracted_text,
-					'from_number' => $from_number,
-					'ist_timestamp' => $ist_timestamp,
-					'screenshot_image' => $screenshot_image,
-					'sender_name_place' => $sender_name_place,
-					'timestamp' => $timestamp,
-					'vision_text' => $vision_text,
-					'reply_id'=>$reply_id,
+					'vno' => $vno,
+					'tagno' => $tagno,
+					'gstvno' => $gstvno,
+					'pickedby' => $pickedby,
+					'checkedby' => $checkedby,
+					'deliverby' => $deliverby,
+					'amt' => $amt,
+					'taxamt'=>$taxamt,
+					'acno'=>$acno,
+					'chemist_id'=>$chemist_id,
+					'status'=>$status,
+					'insert_time'=>$insert_time,
 				);
 
-				if (!empty($message_id)) {
+				if (!empty($gstvno)) {
 					// Check karo agar record already exist karta hai
-					$existing_record = $this->BankModel->select_row("tbl_whatsapp_message", array('message_id' => $message_id));
+					$existing_record = $this->BankModel->select_row("tbl_invoice", array('gstvno' => $gstvno));
 			
 					if ($existing_record) {
 						// Agar record exist karta hai to update karo
-						$where = array('message_id' => $message_id);
-						$this->BankModel->edit_fun("tbl_whatsapp_message", $dt, $where);
+						$where = array('gstvno' => $gstvno);
+						$this->BankModel->edit_fun("tbl_invoice", $dt, $where);
 					} else {
 						// Agar record exist nahi karta hai to insert karo
-						$this->BankModel->insert_fun("tbl_whatsapp_message", $dt);
+						$this->BankModel->insert_fun("tbl_invoice", $dt);
 					}
 				}
 			}
