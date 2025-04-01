0353f3c7c055fcfbf1fdfe987ed8388062727da2
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2025-04-01 14:26:16 +0530

    add

--- application/models/model-bank/BankWhatsAppModel.php
+++ application/models/model-bank/BankWhatsAppModel.php
@@ -407,7 +407,7 @@ class BankWhatsAppModel extends CI_Model
 
 	public function whatsapp_insert_in_processing(){
 
-		$result = $this->BankModel->select_query("SELECT p.id, wm.id AS whatsapp_id, wm.body AS body, wm.vision_text, wm.timestamp, wm.from_number, p.from_text_find_chemist FROM tbl_bank_processing AS p JOIN tbl_whatsapp_message wm ON p.upi_no = wm.upi_no AND wm.date BETWEEN DATE_SUB(p.date, INTERVAL 1 DAY) AND DATE_ADD(p.date, INTERVAL 1 DAY) WHERE p.whatsapp_id = 37130 ORDER BY RAND() LIMIT 25");
+		$result = $this->BankModel->select_query("SELECT p.id, wm.id AS whatsapp_id, wm.body AS body, wm.vision_text, wm.timestamp, wm.from_number, p.from_text_find_chemist FROM tbl_bank_processing AS p JOIN tbl_whatsapp_message wm ON p.upi_no = wm.upi_no AND wm.date BETWEEN DATE_SUB(p.date, INTERVAL 1 DAY) AND DATE_ADD(p.date, INTERVAL 1 DAY) WHERE wm.id = 37130 ORDER BY RAND() LIMIT 25");
 		$result = $result->result();
 		foreach($result as $row) {
 
