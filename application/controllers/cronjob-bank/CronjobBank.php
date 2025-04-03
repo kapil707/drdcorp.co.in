48a1ec6d3dbaedb9374dc4ba440a4295f7a17dd4
kapil707 <kapilsharma@kapils-MacBook-Air.local>
2025-04-03 09:21:48 +0530

    add

--- application/controllers/cronjob-bank/CronjobBank.php
+++ application/controllers/cronjob-bank/CronjobBank.php
@@ -64,6 +64,95 @@ class CronjobBank extends CI_Controller
 		$find_chemist_id = substr($find_chemist_id, 0, -4);
 		echo $find_chemist_id; */
 		$text = "NEFT IN UTR CITIN25543558586 FROM MAXIMUM HELP PHA RMACY HDFCH00 152194269TXN REF NO 0001MAXIMUM HELP PHARM";
+
+		$text = str_replace(array("\r", "\n"), ' ', $text);
+		$from_text = "";
+
+		/**********************************************/
+		$text = preg_replace("/KKBKH\d+/", "", $text);
+		$text = preg_replace("/KK\s*BKH\d+/", "", $text);
+		$text = preg_replace("/KKB\s*KH\d+/", "", $text);
+		$text = preg_replace("/KKBK\s*H\d+/", "", $text);
+		$text = preg_replace("/KKBKH\s*\d+/", "", $text);
+
+		$text = preg_replace("/9300966180/", '', $text);
+		$text = preg_replace("/\s*9300966180/", '', $text);
+		$text = preg_replace("/9\s*300966180/", '', $text);
+		$text = preg_replace("/93\s*00966180/", '', $text);
+		$text = preg_replace("/930\s*0966180/", '', $text);
+		$text = preg_replace("/9300\s*966180/", '', $text);
+		$text = preg_replace("/93009\s*66180/", '', $text);
+		$text = preg_replace("/930096\s*6180/", '', $text);
+		$text = preg_replace("/9300966\s*180/", '', $text);
+		$text = preg_replace("/93009661\s*80/", '', $text);
+		$text = preg_replace("/930096618\s*0/", '', $text);
+
+		$text = preg_replace("/N\s*0/", '', $text);
+		$text = preg_replace("/N2632432758889/", '', $text);
+		$text = preg_replace("/N\s*2632432758889/", '', $text);
+		$text = preg_replace("/N2\s*632432758889/", '', $text);
+		$text = preg_replace("/N26\s*32432758889/", '', $text);
+		$text = preg_replace("/N263\s*2432758889/", '', $text);
+		$text = preg_replace("/N2632\s*432758889/", '', $text);
+		$text = preg_replace("/N26324\s*32758889/", '', $text);
+		$text = preg_replace("/N263243\s*2758889/", '', $text);
+		$text = preg_replace("/N2632432\s*758889/", '', $text);
+		$text = preg_replace("/N26324327\s*58889/", '', $text);
+		$text = preg_replace("/N263243275\s*8889/", '', $text);
+		$text = preg_replace("/N2632432758\s*889/", '', $text);
+		$text = preg_replace("/N26324327588\s*89/", '', $text);
+		$text = preg_replace("/N263243275888\s*9/", '', $text);
+		$text = preg_replace("/N2632432758889\s*/", '', $text);
+
+		$text = preg_replace("/AXOMB2639/", '', $text);
+		$text = preg_replace("/A\s*XOMB2639/", '', $text);
+		$text = preg_replace("/AX\s*OMB2639/", '', $text);
+		$text = preg_replace("/AXO\s*MB2639/", '', $text);
+		$text = preg_replace("/AXOM\s*B2639/", '', $text);
+		$text = preg_replace("/AXOMB\s*2639/", '', $text);
+		$text = preg_replace("/AXOMB2\s*639/", '', $text);
+		$text = preg_replace("/AXOMB26\s*39/", '', $text);
+		$text = preg_replace("/AXOMB263\s*9/", '', $text);
+		$text = preg_replace("/AXOMB2639\s*/", '', $text);
+		/**********************************************/
+		$text = preg_replace('/\s+\d+TXN\s+REF NO/', ' REF NO', $text);
+		$text = preg_replace('/\s+\d+\s+REF NO/', ' REF NO', $text);
+		$text = preg_replace('/AX.*?REF NO/', ' REF NO', $text);
+		$text = preg_replace('/N00.*?REF NO/', ' REF NO', $text);
+		$text = preg_replace('/PUNBY.*?REF NO/', ' REF NO', $text);
+		$text = preg_replace('/PUNBH.*?REF NO/', ' REF NO', $text);
+		$text = preg_replace('/INDBN.*?REF NO/', ' REF NO', $text);
+		$text = preg_replace('/INDBH.*?REF NO/', ' REF NO', $text);
+		$text = preg_replace('/IDFBH.*?REF NO/', ' REF NO', $text); 
+		$text = preg_replace('/ID FBH.*?REF NO/', ' REF NO', $text); 
+		$text = preg_replace('/ICIN.*?REF NO/', ' REF NO', $text);
+		$text = preg_replace('/YES.*?REF NO/', ' REF NO', $text);
+		$text = preg_replace('/POD.*?REF NO/', ' REF NO', $text);
+		$text = preg_replace('/TXN.*?REF NO/', ' REF NO', $text);
+		$text = preg_replace('/FOR.*?REF NO/', ' REF NO', $text);
+		$text = preg_replace('/CNRBH.*?REF NO/', ' REF NO', $text);
+		$text = preg_replace('/N 06.*?REF NO/', ' REF NO', $text);
+		$text = preg_replace('/N06.*?REF NO/', ' REF NO', $text);
+		$text = preg_replace('/SBIN.*?REF NO/', ' REF NO', $text);
+		$text = preg_replace('/BKIDN.*?REF NO/', ' REF NO', $text);
+		$text = preg_replace('/BINH.*?REF NO/', ' REF NO', $text);
+		$text = preg_replace('/C\s*BINH.*?REF NO/', ' REF NO', $text);
+
+		$text = preg_replace('/INDBH.*?REF NO/', ' REF NO', $text);
+		$text = preg_replace('/I\sNDBH.*?REF NO/', ' REF NO', $text);
+		$text = preg_replace('/IN\sDBH.*?REF NO/', ' REF NO', $text);
+		$text = preg_replace('/IND\sBH.*?REF NO/', ' REF NO', $text);
+		$text = preg_replace('/INDB\sH.*?REF NO/', ' REF NO', $text);
+
+		$text = preg_replace('/HDFCH.*?REF NO/', ' REF NO', $text);
+		$text = preg_replace('/H DFCH.*?REF NO/', ' REF NO', $text);
+		$text = preg_replace('/HD FCH.*?REF NO/', ' REF NO', $text);
+		$text = preg_replace('/HDF CH.*?REF NO/', ' REF NO', $text);
+		$text = preg_replace('/HDFC H.*?REF NO/', ' REF NO', $text);
+
+		$text = preg_replace('/SB2.*?-UPI/', ' UPI', $text);
+		echo "<br>".$text;
+
 		preg_match("/FROM\s+(.+?)\s+REF/", $text, $matches);
 		if (!empty($matches) && empty($received_from)){
 			echo $received_from = trim($matches[1]);
