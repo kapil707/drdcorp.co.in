9d08518fbfda82c80bacdc66519eb95113034000
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2025-04-05 15:17:57 +0530

    add

--- application/controllers/cronjob-bank/CronjobBank.php
+++ application/controllers/cronjob-bank/CronjobBank.php
@@ -47,7 +47,7 @@ class CronjobBank extends CI_Controller
 		for($i=16;$i>0;$i--){
 			echo $i;
 			echo "<br>";
-			echo $upi_no = substr($upi_no,"5");
+			echo $upi_no = substr($upi_no,"$i");
 			echo "<br>";
 			echo $text = str_replace($upi_no, ' ', $text);
 			echo "<br>";
