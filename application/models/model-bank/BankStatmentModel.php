04cbec6a405e0b7fcce65990616c268068a952f3
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2025-04-05 16:48:22 +0530

    add

--- application/models/model-bank/BankStatmentModel.php
+++ application/models/model-bank/BankStatmentModel.php
@@ -20,8 +20,8 @@ class BankStatmentModel extends CI_Model
 			$amount = $row->amount;
 			$date = $row->date;
 			$text = $statment_text = trim($row->narrative);
-			$text = str_replace(["\n", "\r",]," ", $text);
-			$text = preg_replace('/\s*\n/', ' ', $text);
+			$text = str_replace(["\n", "\r",],"", $text);
+			$text = preg_replace('/\s*\n/', '', $text);
 			//$text = str_replace($upi_no, ' ', $text);
 			$text = str_replace(' TXN REF NO', ' TXN REF NO', $text);
 			$text = str_replace('T XN REF NO', ' TXN REF NO', $text);
