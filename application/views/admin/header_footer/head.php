c2a0e63aa47272df5ab49ae1aea4724b4529c582
kapil-asus <kapil707sharma@gmail.com>
2025-04-11 11:05:30 +0530

    add

--- application/views/admin/header_footer/head.php
+++ application/views/admin/header_footer/head.php
@@ -6,5 +6,5 @@
 		<title><?= $this->Scheme_Model->get_website_data("title") ;?> || <?= $title1;?></title>
 		<meta name="description" content="overview &amp; stats" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-        <link rel="shortcut icon" href="<?= base_url()?>/uploads/manage_website/photo/<?= $this->Scheme_Model->get_website_data("icon") ;?>" type="image/x-icon">
-        <link rel="icon" href="<?= base_url()?>/uploads/manage_website/photo/<?= $this->Scheme_Model->get_website_data("icon") ;?>" type="image/x-icon">
+        <link rel="shortcut icon" href="<?= base_url()?>/uploads/manage_website/photo/main/<?= $this->Scheme_Model->get_website_data("icon") ;?>" type="image/x-icon">
+        <link rel="icon" href="<?= base_url()?>/uploads/manage_website/photo/main/<?= $this->Scheme_Model->get_website_data("icon") ;?>" type="image/x-icon">
