3692b47133d2ff0ea9f8cc8b7ff8d32bbfa0f8be
kapil-asus <kapil707sharma@gmail.com>
2025-04-14 14:27:25 +0530

    add

--- application/models/model-bank/BankProcessingModel.php
+++ application/models/model-bank/BankProcessingModel.php
@@ -197,7 +197,7 @@ class BankProcessingModel extends CI_Model
 		
 		$date = date("Y-m-d");
 		// jab invoice or whatsapp mil jaya but find chemist ek say jada hoya to yha automatick user ko add karta ha v153 || v155 = v155
-		$result = $this->BankModel->select_query("SELECT id,invoice_chemist FROM `tbl_bank_processing` WHERE `invoice_chemist`=`whatsapp_chemist` and invoice_chemist!='' and whatsapp_chemist!='' and invoice_chemist!=from_text_find_chemist and date='$date'");
+		$result = $this->BankModel->select_query("SELECT id,invoice_chemist FROM `tbl_bank_processing` WHERE `invoice_chemist`=`whatsapp_chemist` and invoice_chemist!='' and whatsapp_chemist!='' and invoice_chemist!=from_text_find_chemist and date='$date' and recommended_status=0");
 		$result = $result->result();
 		foreach($result as $row){
 			if(!empty($row->id)){
