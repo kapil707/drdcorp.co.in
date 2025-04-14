060538d4830a8f996fff720c3dc8126f9e5b2e44
kapil-asus <kapil707sharma@gmail.com>
2025-04-14 14:39:38 +0530

    add

--- application/models/model-bank/BankProcessingModel.php
+++ application/models/model-bank/BankProcessingModel.php
@@ -253,6 +253,7 @@ class BankProcessingModel extends CI_Model
 						'invoice_id'=>'',
 						'invoice_chemist'=>'',
 						'invoice_text'=>'',
+						'from_text_find_chemist'=>$chemist_id
 					);
 					$this->BankModel->edit_fun("tbl_bank_processing", $dt,$where);
 
