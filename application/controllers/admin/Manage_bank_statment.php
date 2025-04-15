fd9039fdb24e637cad386694798ce1c4c61a1f8f
kapil-asus <kapil707sharma@gmail.com>
2025-04-15 11:55:29 +0530

    add

--- application/controllers/admin/Manage_bank_statment.php
+++ application/controllers/admin/Manage_bank_statment.php
@@ -1,6 +1,5 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-require 'vendor/autoload.php';
 
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
