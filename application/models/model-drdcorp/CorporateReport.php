7de1d9a6e28f04e999561d503ffa1ad7b7b9a044
kapil-asus <kapil707sharma@gmail.com>
2025-04-15 12:20:48 +0530

    add

--- application/models/model-drdcorp/CorporateReport.php
+++ application/models/model-drdcorp/CorporateReport.php
@@ -142,13 +142,13 @@ class CorporateReport extends CI_Model
 
 		$email->IsHTML(true);
 
-		$email->isSMTP();
-		$email->Host       = 'mail.drdcorp.co.in'; // SMTP Server
-		$email->SMTPAuth   = true; // Enable SMTP Authentication
-		$email->Username   = 'report@drdcorp.co.in'; // SMTP Username
-		$email->Password   = 'Kapil1234!@#$'; // SMTP Password
-		$email->SMTPSecure = 'ssl'; // Use SSL (as your SMTP port is 465)
-		$email->Port       = 465; // SMTP Port
+		// $email->isSMTP();
+		// $email->Host       = 'mail.drdcorp.co.in'; // SMTP Server
+		// $email->SMTPAuth   = true; // Enable SMTP Authentication
+		// $email->Username   = 'report@drdcorp.co.in'; // SMTP Username
+		// $email->Password   = 'Kapil1234!@#$'; // SMTP Password
+		// $email->SMTPSecure = 'ssl'; // Use SSL (as your SMTP port is 465)
+		// $email->Port       = 465; // SMTP Port
 
 		if($email->send()){
 			$message_status = 'Message has been sent';
