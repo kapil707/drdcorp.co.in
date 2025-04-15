b76bef991707289e679abb56fd257e430ba57d55
kapil-asus <kapil707sharma@gmail.com>
2025-04-15 11:27:27 +0530

    add

--- application/controllers/cronjob-drdcorp/CronjobCorporateReport.php
+++ application/controllers/cronjob-drdcorp/CronjobCorporateReport.php
@@ -1,5 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+
 class CronjobCorporateReport extends CI_Controller 
 {
 	public $email;
--- application/libraries/Phpmailer_lib.php
+++ application/libraries/Phpmailer_lib.php
@@ -1,22 +1,14 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-class PHPMailer_Lib
-{
-    public function __construct(){
-        log_message('Debug', 'PHPMailer class is loaded.');
-    }
 
-    public function load(){
-        // Include PHPMailer library files
-        require_once APPPATH.'third_party/PHPMailer/Exception.php';
-        require_once APPPATH.'third_party/PHPMailer/PHPMailer.php';
-        require_once APPPATH.'third_party/PHPMailer/SMTP.php';
-        
-        $mail = new PHPMailer;
+require APPPATH . 'third_party/PHPMailer/src/Exception.php';
+require APPPATH . 'third_party/PHPMailer/src/PHPMailer.php';
+require APPPATH . 'third_party/PHPMailer/src/SMTP.php';
+
+class Phpmailer_lib {
+    public function load() {
+        $mail = new PHPMailer(true);
         return $mail;
     }
 }
