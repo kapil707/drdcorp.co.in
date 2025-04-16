c0e73df4a59b656500aaef68f6d00b2bf7d03c3c
kapil-asus <kapil707sharma@gmail.com>
2025-04-16 12:49:14 +0530

    add

--- application/models/model-bank/BankWhatsAppModel.php
+++ application/models/model-bank/BankWhatsAppModel.php
@@ -530,9 +530,9 @@ class BankWhatsAppModel extends CI_Model
 				$result11 = $this->BankModel->select_query("SELECT body FROM tbl_whatsapp_message WHERE from_number = '$from_number' AND FROM_UNIXTIME(timestamp) BETWEEN DATE_SUB(FROM_UNIXTIME($timestamp_main), INTERVAL 7 MINUTE) AND DATE_ADD(FROM_UNIXTIME($timestamp_main), INTERVAL 7 MINUTE) and body!='' LIMIT 25 OFFSET 0;");
 				$result11 = $result11->result();
 				foreach($result11 as $row11){
-					$whatsapp_recommended_new.= $row11->body.",";
+					$whatsapp_recommended_new.= $row11->body." || ";
 				}
-				$whatsapp_recommended_new = substr($whatsapp_recommended_new, 0, -1);
+				$whatsapp_recommended_new = substr($whatsapp_recommended_new, 0, -4);
 				$whatsapp_recommended = $whatsapp_recommended_new;
 			}
 
