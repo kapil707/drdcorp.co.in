331b3b2cea8f2a934e9a54c5f68e1f119c8dbb62
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2025-07-02 11:29:04 +0530

    add

--- application/models/model-bank/BankWebhookModel.php
+++ application/models/model-bank/BankWebhookModel.php
@@ -2,6 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 class BankWebhookModel extends CI_Model {
     public function insert_message($data) {
-        return $this->db->insert('webhook_messages', $data);
+        $db_bank = $this->load->database('bank_db', TRUE);
+        return $db_bank->insert('webhook_messages', $data);
     }
 }
