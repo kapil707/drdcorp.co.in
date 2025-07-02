f60f483fed2fd467719f2de5d220f35f6183b278
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2025-07-02 11:27:38 +0530

    add

--- application/controllers/webhook/HookTest.php
+++ application/controllers/webhook/HookTest.php
@@ -5,7 +5,7 @@ class HookTest extends CI_Controller
 	public function __construct(){
 		parent::__construct();
         //Load model
-        $this->load->model('BankWebhookModel');
+        $this->load->model('model-bank/BankWebhookModel');
 	}
 
 	public function upload(){
