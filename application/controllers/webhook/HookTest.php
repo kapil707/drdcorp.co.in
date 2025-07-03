387fed136860a26316ca648569f104769ace90d8
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2025-07-03 15:13:31 +0530

    add

--- application/controllers/webhook/HookTest.php
+++ application/controllers/webhook/HookTest.php
@@ -102,8 +102,275 @@ class HookTest extends CI_Controller
             }
 
             $text = str_replace("Here's a transcription of the visible text in the image:", '', $text);
+
+            $upi_no = "";
+			$amount = "0.0";
+			//********amount********** */
+			// Regular Expression to extract amount.
+			preg_match('/[₹\?]([\d,]+)/', $text, $matches);
+			// Check if match is found
+			if (!empty($matches[1])) {
+				$amount = $matches[1];
+			}
+			if($amount == "0.0"){
+				preg_match('/[₹\?]([\d,]+(?:\.\d{1,2})?)/', $text, $matches);
+				// Check if match is found
+				if (!empty($matches[1])) {
+					$amount = $matches[1];
+				}
+			}
+			if($amount == "0.0"){
+				preg_match('/Amount:\s*([\d,]+)/', $text, $matches);
+				// Check if match is found
+				if (!empty($matches[1])) {
+					$amount = $matches[1];
+				}
+			}
+			if($amount == "0.0"){
+				preg_match('/\*\*Transfer Amount:\*\* ([\d,]+\.\d{2})/', $text, $matches);
+				// Check if match is found
+				if (!empty($matches[1])) {
+					$amount = $matches[1];
+				}
+			}
+			if($amount == "0.0"){
+				preg_match('/\*\*Transfer Amount:\*\* ([\d,]+\.\d{2})/', $text, $matches);
+				// Check if match is found
+				if (!empty($matches[1])) {
+					$amount = $matches[1];
+				}
+			}
+			if($amount == "0.0"){
+				preg_match_all('/\?[\s]*([\d,.]+)/', $text, $matches);
+
+				if (!empty($matches[1])) {
+					$amount = !empty($matches[1][0]) ? $matches[1][0] : $matches[1][1] ?? '0.0';
+				}
+			}
+
+			if($amount == "0.0"){
+				preg_match('/Rs ([\d,]+\.?\d{0,2})/', $text, $matches);
+				// Check if match is found
+				if (!empty($matches[1])) {
+					$amount = $matches[1];
+				}
+			}
+
+			if($amount == "0.0"){
+				preg_match('/INR ([\d,]+\.?\d{0,2})/', $text, $matches);
+				// Check if match is found
+				if (!empty($matches[1])) {
+					$amount = $matches[1];
+				}
+			}
+
+			if($amount == "0.0"){
+				preg_match('/\bRs\.\s?(\d{1,3}(?:,\d{3})*\.\d{2})\b/', $text, $matches);
+				// Check if match is found
+				if (!empty($matches[1])) {
+					$amount = $matches[1];
+				}
+			}
+
+			if($amount == "0.0"){
+				preg_match('/\b(\d{1,3}(?:,\d{3})*\.\d{2})\b/', $text, $matches);
+				// Check if match is found
+				if (!empty($matches[1])) {
+					$amount = $matches[1];
+				}
+			}
+
+			$type = 0;
+			/************************************************** */
+			// Regular Expression to extract UTR No.
+			preg_match('/Reference No\. \(UTR No\.\/RRN\): (\S+)/', $text, $matches);
+			// Check if match is found
+			if (!empty($matches[1])) {
+				$upi_no = $matches[1];
+				$type = 1;
+				//echo "UTR Number: " . $matches[1]; // Output: KKBKH25070930804
+			}
+
+			if(empty($upi_no)){
+				preg_match('/UTR:\s*(\d+)/', $text, $matches);
+				if (!empty($matches[1])) {
+					$upi_no = $matches[1];
+					$type = 2;
+					//echo "UTR Number: " . $matches[1];
+				}
+			}
+
+			if(empty($upi_no)){
+				preg_match('/UPI transaction ID:\s*(\d+)/', $text, $matches);
+				if (!empty($matches[1])) {
+					$upi_no = $matches[1];
+					$type = 3;
+					//echo "UTR Number: " . $matches[1];
+				}
+			}
+
+			if(empty($upi_no)){
+				preg_match('/Transaction ID:\s*([\w\d]+)/', $text, $matches);
+				if (!empty($matches[1])) {
+					$upi_no = $matches[1];
+					$type = 4;
+					//echo "UTR Number: " . $matches[1];
+				}
+			}
+
+			if(empty($upi_no)){
+				preg_match('/UPI Ref\. No:\s*([\d\s]+)/', $text, $matches);
+				if (!empty($matches[1])) {
+					$upi_no = $matches[1];
+					$type = 5;
+					//echo "UTR Number: " . $matches[1];
+				}
+			}
+
+			if(empty($upi_no)){
+				preg_match('/UPI txn id:\s*(\d+)/', $text, $matches);
+				if (!empty($matches[1])) {
+					$upi_no = $matches[1];
+					$type = 6;
+					//echo "UTR Number: " . $matches[1];
+				}
+			}
+
+			if(empty($upi_no)){
+				preg_match('/UPI Ref ID:\s*(\d+)/', $text, $matches);
+				if (!empty($matches[1])) {
+					$upi_no = $matches[1];
+					$type = 7;
+					//echo "UTR Number: " . $matches[1];
+				}
+			}
+
+			if(empty($upi_no)){
+				preg_match('/UPI transaction ID\s*(\d+)/', $text, $matches);
+				if (!empty($matches[1])) {
+					$upi_no = $matches[1];
+					$type = 8;
+					//echo "UTR Number: " . $matches[1];
+				}
+			}
+
+			if(empty($upi_no)){
+				preg_match('/UPI Ref\. No:\s*([\d\s]+)/', $text, $matches);
+				if (!empty($matches[1])) {
+					$upi_no = $matches[1];
+					$type = 9;
+					//echo "UTR Number: " . $matches[1];
+				}
+			}
+
+			if(empty($upi_no)){
+				// Regex se UPI Reference Number extract karna
+				preg_match('/\*\*UPI Ref\. No:\*\*\s*([\d\s]+)/', $text, $matches);
+
+				if (!empty($matches[1])) {
+					$upi_no = preg_replace('/\s+/', '', $matches[1]); // Space remove karna
+					$type = 10;
+					//echo "UTR Number: " . $upi_no;
+				}
+			}
+
+			if(empty($upi_no)){
+				// Regex se UPI Reference Number extract karna
+				preg_match('/\*\*\s*Reference No\. \(UTR No\.\/RRN\):\s*\*\*\s*(\w+\d+)/', $text, $matches);
+				if (!empty($matches[1])) {
+					$upi_no = $matches[1];
+					$type = 11;
+					//echo "UTR Number: " . $upi_no;
+				}
+			}
+
+			if(empty($upi_no)){
+				// Regex se Transaction ID extract karna
+				preg_match('/\b\d{12}\b/', $text, $matches);
+				if (!empty($matches[0])) {
+					$upi_no = $matches[0];
+					$type = 12;
+					//echo "UTR Number: " . $upi_no;
+				}
+			}
+
+			if(empty($upi_no)){
+				// Regex se Transaction ID extract karna
+				preg_match('/\*\*Transaction ID:\*\*\s*(\d+)/', $text, $matches);
+				if (!empty($matches[1])) {
+					$upi_no = $matches[1];
+					$type = 13;
+					//echo "UTR Number: " . $upi_no;
+				} 
+			}
+
+			if(empty($upi_no)){
+				// Regex se Transaction ID extract karna
+				preg_match('/Transaction ID\s*\n*([\w\d]+)/', $text, $matches);
+				if (!empty($matches[1])) {
+					$upi_no = $matches[1];
+					$type = 14;
+					//echo "UTR Number: " . $upi_no;
+				} 
+			}
+
+			if(empty($upi_no)){
+				// Regex se Transaction ID extract karna
+				preg_match('/\*\*Reference Number:\*\*\s*([\w\d]+)/', $text, $matches);
+				if (!empty($matches[1])) {
+					$upi_no = $matches[1];
+					$type = 15;
+					//echo "UTR Number: " . $upi_no;
+				}
+			}
+
+			if(empty($upi_no)){
+				// Regex se Transaction ID extract karna
+				preg_match('/Reference\s*Number\s*([A-Z0-9]+)/i', $text, $matches);
+				if (!empty($matches[1])) {
+					$upi_no = $matches[1];
+					$type = 16;
+					//echo "UTR Number: " . $upi_no;
+				}
+			}
+
+			if(empty($upi_no)){
+				// Regex se Transaction ID extract karna
+				preg_match('/UTR:\s*([\w\d]+)/', $text, $matches);
+				if (!empty($matches[1])) {
+					$upi_no = $matches[1];
+					$type = 17;
+					//echo "UTR Number: " . $upi_no;
+				} 
+			}
+
+			if(empty($upi_no)){
+				// Regex se Transaction ID extract karna
+				preg_match('/Reference Number:\s*([\w\d]+)/', $text, $matches);
+				if (!empty($matches[1])) {
+					$upi_no = $matches[1];
+					$type = 18;
+					//echo "UTR Number: " . $upi_no;
+				} 
+			}
+
+			if(empty($upi_no)){
+				// Regex se Transaction ID extract karna
+				preg_match('/\*\*Transaction ID\*\*\s*(\S+)/', $text, $matches);
+				if (!empty($matches[1])) {
+					$upi_no = $matches[1];
+					$type = 19;
+					//echo "UTR Number: " . $upi_no;
+				} 
+			}
+
+			$amount = str_replace([",", ".00"], "", $amount);
+			$amount = trim($amount);
+
+			$upi_no = trim($upi_no);
+
             $where = array('id'=>$row->id);
-            $dt = array('status'=>'1','gemini_text'=>$text);
+            $dt = array('status'=>'1','gemini_text'=>$text,'upi_no'=>$upi_no,'amount'=>$amount);
             $this->BankWebhookModel->update_message($dt,$where);
         }
     }
