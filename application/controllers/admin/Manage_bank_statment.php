e52eed0896af8c05d19a094e5482d1ab7cbdd7bf
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2025-07-03 08:19:52 +0530

    add

--- application/controllers/admin/Manage_bank_statment.php
+++ application/controllers/admin/Manage_bank_statment.php
@@ -488,16 +488,17 @@ class Manage_bank_statment extends CI_Controller {
 		
 		if(isset($_POST["checkbox-delete"])){
 			$essysol_id2 = $_POST['essysol_id2'];
-			$query = $this->BankModel->select_query("select bank_processing_id from tbl_bank_essysol where id='$essysol_id2'");
+			$query = $this->BankModel->select_query("select bank_processing_id from tbl_bank_essysol where id='$essysol_id2' and status=0");
 			$row = $query->row();
 			if(!empty($row->bank_processing_id)){
+				
 				$where = array('id' => $row->bank_processing_id,);
 				$dt = array('final_status' => '4',);
 				$this->BankModel->edit_fun("tbl_bank_processing", $dt,$where);
-			}
 
-			$where = array('id' => $essysol_id2,);
-			$this->BankModel->delete_fun("tbl_bank_essysol",$where);
+				$where2 = array('id' => $essysol_id2,);
+				$this->BankModel->delete_fun("tbl_bank_essysol",$where2);
+			}
 		}
 
 		$query = $this->BankModel->select_query("SELECT be.essysol_id,be.status as essysol_status,be.id as essysol_id2,s.*,p.final_chemist as chemist_id,p.invoice_text as invoice_number,p.id as pid,p.final_status from tbl_statment as s left JOIN tbl_bank_processing as p on p.upi_no=s.customer_reference left join tbl_bank_essysol as be on p.upi_no=be.upi_no where s.date BETWEEN '$start_date' AND '$end_date' order by s.id asc");
