0edf15499faddaa3db6b328fa128440350cb8030
kapil sharma <kapilsharma@kapils-Mac-mini.local>
2025-07-03 14:58:02 +0530

    add

--- application/controllers/webhook/HookTest.php
+++ application/controllers/webhook/HookTest.php
@@ -53,13 +53,13 @@ class HookTest extends CI_Controller
 
     public function test(){
 
+        $apiKey = $this->Scheme_Model->get_website_data("gemini_apikey");
+        
         $query = $this->BankModel->select_query("SELECT * FROM `webhook_messages` where media_id!='' and status=0 limit 1");
 	    $row = $query->row();
         if(!empty($row->id)){
 
-            $image_url = 'https://api.wassi.chat/v1/chat/66faf180345d460e9984e4ac/files/'.$row->media_id.'/download?token=531fe5caf0e132bdb6000bf01ed66d8cfb75b53606cc8f6eed32509d99d74752f47f288db155557e'; // REPLACE with the actual $imageData = base64_encode(file_get_contents($imagePath));
-
-            $apiKey = $this->Scheme_Model->get_website_data("gemini_apikey");
+            $image_url = 'https://api.wassi.chat/v1/chat/66faf180345d460e9984e4ac/files/'.$row->media_id.'/download?token=531fe5caf0e132bdb6000bf01ed66d8cfb75b53606cc8f6eed32509d99d74752f47f288db155557e';
 
             $imageData = base64_encode(file_get_contents($image_url));
 
